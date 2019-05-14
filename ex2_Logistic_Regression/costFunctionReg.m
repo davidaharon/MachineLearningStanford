5c6324b546d0d63a712457927b1bbf449f14c908
David Aharon <davidahar@gmail.com>
2019-05-14 21:17:28 +0300

    ex2 implementation

--- ex2_Logistic_Regression/costFunctionReg.m
+++ ex2_Logistic_Regression/costFunctionReg.m
@@ -17,9 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[J, grad] = costFunction(theta, X, y);
 
+J = J + lambda/(2*m) * sum(theta(2:end) .^ 2);
 
-
+grad(2:end) = grad(2:end) + (lambda/m)*theta(2:end);
 
 
 % =============================================================
--- ex2_Logistic_Regression/predict.m
+++ ex2_Logistic_Regression/predict.m
@@ -14,12 +14,9 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
-
+p = zeros(m, 1);
+positions = find(sigmoid(X*theta) >= 0.5); 
+p(positions) = 1;
 
 % =========================================================================
 
